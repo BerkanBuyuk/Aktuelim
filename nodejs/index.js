dd0578537f4718078e9300dac108678cf7815c81
Berkan Buyuk <berkanbyk55@gmail.com>
2024-01-08 13:27:02 +0300

    .env düzenlendi.

--- nodejs/index.js
+++ nodejs/index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 
 ////GET MARKETS
 app.get("/api/markets", (req, res) => {
-  const q = "SELECT * FROM Markets";
+  const q = `${process.env.GET_MARKETS_QUERY}`;
   db.query(q, (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
@@ -31,8 +31,7 @@ app.get("/api/markets", (req, res) => {
 
 //POST MARKETS
 app.post("/api/markets", (req, res) => {
-  const q =
-    "INSERT INTO Markets (`market_id`, `market_name`, `market_image`, `category_id`) VALUES (?, ?, ?)";
+  const q = `${process.env.POST_MARKETS_QUERY}`;
   const values = [
     req.body.market_id,
     req.body.market_name,
@@ -53,7 +52,7 @@ app.listen(port, () => {
 
 //GET CATALOGS
 app.get("/api/catalogs", (req, res) => {
-  const q = "SELECT * FROM Catalogs";
+  const q = `${process.env.GET_CATALOGS_QUERY}`;
   db.query(q, (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
@@ -70,9 +69,7 @@ app.post("/api/catalogs", (req, res) => {
     market_id,
   } = req.body;
 
-  const sql =
-    "INSERT INTO Catalogs (catalog_id, catalog_title, catalog_image, catalog_description, market_id) VALUES (?, ?, ?, ?, ?)";
-
+  const sql = `${process.env.POST_CATALOGS_QUERY}`;
   db.query(
     sql,
     [catalog_id, catalog_title, catalog_image, catalog_description, market_id],
@@ -90,7 +87,7 @@ app.post("/api/catalogs", (req, res) => {
 
 //GET FAVORITES
 app.get("/api/favorites", (req, res) => {
-  const q = "SELECT * FROM Favorites";
+  const q = `${process.env.GET_FAVORITES_QUERY}`;
   db.query(q, (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
@@ -103,13 +100,13 @@ app.get("/api/favorites", (req, res) => {
 app.post("/api/favorites", (req, res) => {
   const { favori_id, catalog_id } = req.body;
 
-  const sql = "INSERT INTO Favorites (favori_id, catalog_id) VALUES (?, ?)";
+  const sql = `${process.env.POST_FAVORITES_QUERY}`;
 
   db.query(sql, [favori_id, catalog_id], (err, result) => {
     if (err) {
       res.status(500).send("Sorgu hatası");
     } else {
-      res.status(200).send("Yeni katalog başarıyla eklendi.");
+      res.status(200).send("Yeni favori katalog başarıyla eklendi.");
     }
   });
 });
@@ -118,7 +115,7 @@ app.post("/api/favorites", (req, res) => {
 
 //GET CATEGORIES
 app.get("/api/categories", (req, res) => {
-  const q = "SELECT * FROM Categories";
+  const q = `${process.env.GET_CATEGORIES_QUERY}`;
   db.query(q, (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
