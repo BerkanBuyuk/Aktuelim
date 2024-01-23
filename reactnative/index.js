0be4fa296cc074dcfa589aadd48b4c7850c182a3
Berkan Buyuk <berkanbyk55@gmail.com>
2024-01-23 12:50:14 +0300

    React Native Register Page post işlemleri tamamlandı.

--- reactnative/index.js
+++ reactnative/index.js
@@ -11,13 +11,15 @@ import i18n from './src/assets/locales/services/i18next';
 import {I18nextProvider} from 'react-i18next';
 import {Provider} from 'react-redux';
 import store from './src/redux/store';
+import Register from './src/auth/register';
 
 const Aktuelim = () => (
-  <Provider store={store}>
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
-  </Provider>
+  // <Provider store={store}>
+  //   <I18nextProvider i18n={i18n}>
+  //     <App />
+  //   </I18nextProvider>
+  // </Provider>
+  <Register />
 );
 
 AppRegistry.registerComponent(appName, () => Aktuelim);
---
+++ reactnative/src/auth/register.js
@@ -0,0 +1,71 @@
+import React, {useState} from 'react';
+import {View, Text, TextInput, Button, StyleSheet} from 'react-native';
+import axios from 'axios';
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    user_username: '',
+    user_email: '',
+    user_password: '',
+    user_name: '',
+  });
+
+  const [err, setErr] = useState(null);
+
+  const handleChange = (key, value) => {
+    setFormData({...formData, [key]: value});
+  };
+
+  // console.log(formData);
+
+  const handleClick = async e => {
+    e.preventDefault();
+
+    try {
+      await axios.post('http://localhost:8800/api/auth/register', formData);
+    } catch (err) {
+      setErr(err.response.data);
+    }
+  };
+
+  console.log(err);
+
+  return (
+    <View style={styles.view_style}>
+      <Text>Register</Text>
+      <TextInput
+        placeholder="Username"
+        name="user_username"
+        onChangeText={text => handleChange('user_username', text)}
+      />
+      <TextInput
+        placeholder="Email"
+        name="user_email"
+        onChangeText={text => handleChange('user_email', text)}
+      />
+      <TextInput
+        placeholder="Password"
+        name="user_password"
+        secureTextEntry
+        onChangeText={text => handleChange('user_password', text)}
+      />
+      <TextInput
+        placeholder="Name"
+        name="user_name"
+        onChangeText={text => handleChange('user_name', text)}
+      />
+      <Button title="Register" onPress={handleClick} />
+      {err && <Text>{err}</Text>}
+    </View>
+  );
+};
+
+export default Register;
+
+const styles = StyleSheet.create({
+  view_style: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+});
