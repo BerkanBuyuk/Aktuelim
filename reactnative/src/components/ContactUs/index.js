ca9c6ab78f26c0ec4cb7cf8cf84864f840d85ffe
Berkan Buyuk <berkanbyk55@gmail.com>
2024-01-30 21:46:45 +0300

    NativeWind kuruldu. ContactUs Componentinde uygulandı.

--- reactnative/src/components/ContactUs/index.js
+++ reactnative/src/components/ContactUs/index.js
@@ -1,31 +1,17 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text} from 'react-native';
 import React from 'react';
 import {useSelector} from 'react-redux';
-import Styles from '../../assets/Styles';
 
 const ContactUs = () => {
   const darkMode = useSelector(state => state.theme.darkMode);
   return (
     <View
-      style={[
-        styles.view_style,
-        {
-          backgroundColor: darkMode
-            ? Styles.dark_bg_color
-            : Styles.light_bg_color,
-        },
-      ]}>
+      className={`flex-1 items-center justify-center ${
+        darkMode ? 'bg-dark_bg_color' : 'bg-light_bg_color'
+      }`}>
       <Text>ContactUs</Text>
     </View>
   );
 };
 
 export default ContactUs;
-
-const styles = StyleSheet.create({
-  view_style: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
