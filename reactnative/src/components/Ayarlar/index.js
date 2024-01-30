34c581cf06f5f5066e3f0e90a942a6b04c12757c
Berkan Buyuk <berkanbyk55@gmail.com>
2024-01-30 22:38:26 +0300

    React native modal paketi kaldırıldı.

--- reactnative/src/components/Ayarlar/index.js
+++ reactnative/src/components/Ayarlar/index.js
@@ -1,22 +1,16 @@
-import {View, StyleSheet} from 'react-native';
+import {View} from 'react-native';
 import React from 'react';
 import Theme from '../../assets/Theme';
 import {useSelector} from 'react-redux';
-import Styles from '../../assets/Styles';
 import Language from '../../assets/Language';
 
 const Ayarlar = () => {
   const darkMode = useSelector(state => state.theme.darkMode);
   return (
     <View
-      style={[
-        styles.view_style,
-        {
-          backgroundColor: darkMode
-            ? Styles.dark_bg_color
-            : Styles.light_bg_color,
-        },
-      ]}>
+      className={`flex-1 p-6 ${
+        darkMode ? 'bg-dark_bg_color' : 'bg-light_bg_color'
+      }`}>
       <Theme />
       <Language />
     </View>
@@ -24,12 +18,3 @@ const Ayarlar = () => {
 };
 
 export default Ayarlar;
-
-const styles = StyleSheet.create({
-  view_style: {
-    flex: 1,
-    paddingTop: 20,
-    paddingLeft: 30,
-    paddingRight: 30,
-  },
-});
