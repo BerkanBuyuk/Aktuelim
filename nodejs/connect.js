82d9e00409ac3755044a3fd015514d80879371b8
Berkan Buyuk <berkanbyk55@gmail.com>
2024-01-31 11:11:06 +0300

    render deneme 2

--- nodejs/connect.js
+++ nodejs/connect.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 export const db = mysql.createConnection({
   host: `${process.env.HOST}`,
-  user: "ulcbew6yepkrndxs",
+  user: "root",
   password: `${process.env.PASSWORD}`,
   database: `${process.env.DATABASE}`,
   port: `${process.env.PORT}`,
