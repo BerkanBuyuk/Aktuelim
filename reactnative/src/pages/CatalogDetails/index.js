6979411ae70df6a1dc3de0dc0de56c15dcffc235
Berkan Buyuk <berkanbyk55@gmail.com>
2024-01-31 19:32:50 +0300

    CatalogDetails nativeWind'e dönüştürüldü.

--- reactnative/src/pages/CatalogDetails/index.js
+++ reactnative/src/pages/CatalogDetails/index.js
@@ -1,39 +1,20 @@
-import {View, Image, StyleSheet} from 'react-native';
+import {View, Image} from 'react-native';
 import React from 'react';
 import {useSelector} from 'react-redux';
-import Styles from '../../assets/Styles';
 
 const CatalogDetails = ({route}) => {
   const darkMode = useSelector(state => state.theme.darkMode);
   return (
     <View
-      style={[
-        styles.view_style,
-        {
-          backgroundColor: darkMode
-            ? Styles.dark_bg_color
-            : Styles.light_bg_color,
-        },
-      ]}>
+      className={`flex-1 p-2.5 justify-center content-center ${
+        darkMode ? 'bg-dark_bg_color' : 'bg-light_bg_color'
+      }`}>
       <Image
         source={{uri: `${route.params.catalog_image}`}}
-        style={styles.image_style}
+        className="w-full h-[calc(100vh/1.5)]"
       />
     </View>
   );
 };
 
 export default CatalogDetails;
-
-const styles = StyleSheet.create({
-  view_style: {
-    flex: 1,
-    alignContent: 'center',
-    padding: 10,
-    justifyContent: 'center',
-  },
-  image_style: {
-    width: '100%',
-    height: '90%',
-  },
-});
