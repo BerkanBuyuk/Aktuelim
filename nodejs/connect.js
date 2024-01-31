9e3589919abcf7f05e6cb91b9d09d6b9f3b4a480
Berkan Buyuk <berkanbyk55@gmail.com>
2024-01-31 12:05:56 +0300

    render deneme 7

--- nodejs/connect.js
+++ nodejs/connect.js
@@ -2,9 +2,9 @@ import mysql from "mysql";
 import "dotenv/config";
 
 export const db = mysql.createConnection({
-  host: process.env.HOST,
+  host: "bdsz1fcgfjmpymaiymr9-mysql.services.clever-cloud.com",
+  database: "bdsz1fcgfjmpymaiymr9",
   user: "ulcbew6yepkrndxs",
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  port: process.env.PORT,
+  password: "F0TAxDDv8BY9g9ECiQC6",
+  port: 3306,
 });
