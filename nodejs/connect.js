533806a147522cd38fcefd7285a5350e9f31a60f
Berkan Buyuk <berkanbyk55@gmail.com>
2024-02-05 00:27:16 +0300

    firebase deneme 2

--- nodejs/connect.js
+++ nodejs/connect.js
@@ -2,9 +2,9 @@ import mysql from "mysql";
 import "dotenv/config";
 
 export const db = mysql.createConnection({
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  user: process.env.DB_USER,
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
+  host: "bdsz1fcgfjmpymaiymr9-mysql.services.clever-cloud.com",
+  database: "bdsz1fcgfjmpymaiymr9",
+  user: "ulcbew6yepkrndxs",
+  password: "F0TAxDDv8BY9g9ECiQC6",
+  port: 3306,
 });
