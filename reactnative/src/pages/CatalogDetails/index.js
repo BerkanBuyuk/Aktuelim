4c5e0d9b0e5415d38a1c26ae924606760075ef8b
Berkan Buyuk <berkanbyk55@gmail.com>
2024-02-05 12:16:33 +0300

    Rotate iptal edildi.

--- reactnative/src/pages/CatalogDetails/index.js
+++ reactnative/src/pages/CatalogDetails/index.js
@@ -1,18 +1,46 @@
-import {View, Image} from 'react-native';
-import React from 'react';
+import {View, Image, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
 import {useSelector} from 'react-redux';
+import LoadingLoader from '../../components/Loader/loadingLoader';
 
 const CatalogDetails = ({route}) => {
   const darkMode = useSelector(state => state.theme.darkMode);
+  const [loading, setLoading] = useState(true);
+  const [err, setErr] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await fetch(route.params.catalog_image);
+        setLoading(false);
+      } catch (error) {
+        console.error('Veri yüklenirken bir hata oluştu:', error);
+        setErr('Veri yüklenirken bir hata oluştu.');
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [route.params.catalog_image]);
+
   return (
     <View
       className={`flex-1 p-2.5 justify-center content-center ${
         darkMode ? 'bg-dark_bg_color' : 'bg-light_bg_color'
       }`}>
-      <Image
-        source={{uri: `${route.params.catalog_image}`}}
-        className="w-full h-[calc(100vh/1.5)]"
-      />
+      {loading ? (
+        <LoadingLoader />
+      ) : (
+        <>
+          {err ? ( // Yeni eklenen satır
+            <Text className="text-red-600">{err}</Text> // Yeni eklenen satır
+          ) : (
+            <Image
+              source={{uri: `${route.params.catalog_image}`}}
+              className="w-full h-[calc(100vh/1.5)]"
+            />
+          )}
+        </>
+      )}
     </View>
   );
 };
