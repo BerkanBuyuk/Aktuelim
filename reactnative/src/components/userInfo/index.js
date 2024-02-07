b64dd10976edf1165fd760bf40c1e14f2798595f
Berkan Buyuk <berkanbyk55@gmail.com>
2024-02-07 18:35:19 +0300

    Backend register deneme 1

--- reactnative/src/components/userInfo/index.js
+++ reactnative/src/components/userInfo/index.js
@@ -1,8 +1,35 @@
 import {View, Text, Image} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import {USERS_URL} from '@env';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const UserInfo = () => {
+  const [userData, setUserData] = useState();
+
+  const getUsers = async url => {
+    try {
+      const usersToken = await AsyncStorage.getItem('accessToken');
+
+      if (usersToken) {
+        const response = await axios.get(url, {
+          headers: {
+            Authorization: `Bearer ${usersToken}`,
+          },
+        });
+        console.log(response.data[1].user_email);
+      }
+
+      // setUserData(response.data);
+    } catch (error) {
+      console.log('Hata: ', error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers(USERS_URL);
+  }, []);
+
   return (
     <View className="items-center my-5">
       <Image
